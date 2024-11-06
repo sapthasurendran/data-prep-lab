78edd5cfb214910ca67a64bc9a66dfa1457c304f
Sujee Maniyam <sujee@sujee.net>
2024-11-05 23:06:35 -0800

    crawling with DPK connector
    Signed-off-by: Sujee Maniyam <sujee@sujee.net>

---
+++ examples/notebooks/html-processing/README.md
@@ -0,0 +1,28 @@
+# Using Data Prep Kit to Process HTML files
+
+## Step-1: Setup Python Env
+
+See [project README](../../../README.md#create-a-virtual-environment).
+
+Here is a quick setup guide
+
+```bash
+conda create -n dpk-r022dev2-py311  python=3.11
+conda activate dpk-r022dev2-py311
+```
+
+If on linux do this
+
+```bash
+conda install gcc_linux-64
+conda install gxx_linux-64
+```
+
+Install modules
+
+```bash
+pip install -r requirements.txt 
+```
+
+
+## Download HTML pages
---
+++ examples/notebooks/html-processing/my_config.py
@@ -0,0 +1,20 @@
+import os 
+
+## Configuration
+class MyConfig:
+    pass 
+
+MY_CONFIG = MyConfig ()
+
+## Crawl settings
+# MY_CONFIG.CRAWL_URL_BASE = 'https://thealliance.ai/'
+MY_CONFIG.CRAWL_URL_BASE = 'https://thealliance.ai/our-work'
+MY_CONFIG.CRAWL_MAX_DOWNLOADS = 10
+MY_CONFIG.CRAWL_MAX_DEPTH = 2
+MY_CONFIG.CRAWL_MIME_TYPE = 'html'
+
+## Input Data - configure this to the folder we want to process
+MY_CONFIG.INPUT_DIR = "input"
+MY_CONFIG.OUTPUT_DIR = "output"
+MY_CONFIG.OUTPUT_DIR_FINAL = os.path.join(MY_CONFIG.OUTPUT_DIR , "output_final")
+### -------------------------------
