4ecb42040d4bdc4e73b94b21ca0c14803a9f0255
Sujee Maniyam <sujee@node51.com>
2024-11-14 08:44:15 -0800

    html2pq working
    Signed-off-by: Sujee Maniyam <sujee@node51.com>

--- examples/notebooks/html-processing/my_config.py
+++ examples/notebooks/html-processing/my_config.py
@@ -14,7 +14,8 @@ MY_CONFIG.CRAWL_MAX_DEPTH = 2
 MY_CONFIG.CRAWL_MIME_TYPE = 'text/html'
 
 ## Input Data - configure this to the folder we want to process
-MY_CONFIG.INPUT_DIR = "input"
+# MY_CONFIG.INPUT_DIR = "input"
+MY_CONFIG.INPUT_DIR = "input2/thealliance.ai/"
 MY_CONFIG.OUTPUT_DIR = "output"
 MY_CONFIG.OUTPUT_DIR_FINAL = os.path.join(MY_CONFIG.OUTPUT_DIR , "output_final")
 ### -------------------------------
