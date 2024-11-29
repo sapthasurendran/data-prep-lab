06599a502a819193461b8835bb52ab73ce77f047
Aisha Darga <37473748+AishaDarga@users.noreply.github.com>
2024-11-29 14:19:28 +0530

    Update hap_local_python.py
    Signed-off-by: Aisha Mohammed Farooq Darga <aisdarg1@in.ibm.com>

--- transforms/universal/hap/python/src/hap_local_python.py
+++ transforms/universal/hap/python/src/hap_local_python.py
@@ -17,14 +17,9 @@ from data_processing.runtime.pure_python import PythonTransformLauncher
 from data_processing.utils import ParamsUtils
 from hap_transform_python import HAPPythonTransformConfiguration
 
-# create parameters
-# input_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "../test-data/input"))
-# output_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "../output"))
-
 input_folder = os.getenv('INPUT_FOLDER', os.path.abspath(os.path.join(os.path.dirname(__file__), "../test-data/input")))
 output_folder = os.getenv('OUTPUT_FOLDER', os.path.abspath(os.path.join(os.path.dirname(__file__), "../output"))
 )
-print("PATHS: ", input_folder, output_folder)
 
 local_conf = {
     "input_folder": input_folder,
@@ -39,7 +34,6 @@ params = {
     "runtime_code_location": ParamsUtils.convert_to_ast(code_location),
 }
 
-
 hap_params = {
     "model_name_or_path": os.getenv('MODEL_NAME_OR_PATH', 'ibm-granite/granite-guardian-hap-38m'),
     "annotation_column": os.getenv('ANNOTATION_COLUMN', "hap_score"),
@@ -52,7 +46,6 @@ hap_params = {
 if __name__ == "__main__":
     # Set the simulated command line args
     sys.argv = ParamsUtils.dict_to_req(d=params | hap_params)
-    print("PARAMs: ", hap_params)
     # create launcher
     launcher = PythonTransformLauncher(runtime_config=HAPPythonTransformConfiguration())
     # Launch the ray actor(s) to process the input
